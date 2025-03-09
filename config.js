136ca479d770a660670821e807c68006d5a3a1d9
DJDewmina <djdewminalk@gmail.com>
2025-03-09 23:16:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rN4VgZKB#5iwxKDk9iYE22gona2YP6KE1LNyZXRwJIeEUPkqu4fc",
+SESSION_ID: process.env.SESSION_ID || "vF4FwKrZ#5Iay2Um4u984Idv1NoLg8naJpLS5M9YmVKZ602Pt5OI",
 MONGODB: process.env.MONGODB || "mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama"
 };
 
